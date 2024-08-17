8a4f0c94fa1807b5f9cf38bacf1c122367682d4e
sarox0987 <153465797+sarox0987@users.noreply.github.com>
2024-08-17 15:44:11 +0330

    Update custom-allora.sh

--- custom-allora.sh
+++ custom-allora.sh
@@ -44,7 +44,7 @@ cat <<EOF > config.json
         "alloraHomeDir": "",
         "gas": "1000000",
         "gasAdjustment": 1.0,
-        "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+        "nodeRpc": "https://allora-rpc.testnet-1.testnet.allora.network/",
         "maxRetries": 1,
         "delay": 1,
         "submitTx": false
