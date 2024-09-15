1cd516b5e5fe308e1b7dc8d24b4990ccc5cc33e2
sarox0987 <153465797+sarox0987@users.noreply.github.com>
2024-09-15 22:02:34 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rpc="https://allora-rpc.testnet.allora.network"
+#rpc="https://allora-rpc.testnet.allora.network"
+rpc="https://rpc.ankr.com/allora_testnet"
 
 read -p "Enter your worker index: " index
 read -p "Enter your mnemonic phrase: " mnemonic_phrase
